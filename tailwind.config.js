03bb4f807fb726110b9a1770bb7d9a4aae9a6e2b
Xiaoliang Luan <helix_ogg@hotmail.com>
2023-04-15 21:52:34 +0800

    replay tailwind CDN

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'very-dark-blue': 'hsl(233, 47%, 7%)',
+        'dark-desaturated-blue': 'hsl(244, 38%, 16%)',
+        'soft-violet': 'hsl(277, 64%, 61%)',
+      },
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+        'lexend-deca': ['"Lexend Deca"', 'sans-serif']
+      },
+      fontSize: {
+        'body': ['15px', {
+          lineHeight: '1.625',
+          fontWeight: '400',
+        }],
+      },
+    }
   },
   plugins: [],
 }
